90f0b4fc396de9c30c400129c782d9c845b4e8e2
Nathan Leiby <nathan.leiby@clever.com>
2014-11-26 15:30:03 -0800

    json_encoder - interpolate any value from json to message_fields
    - instead of just strings before, we now stringify all JSON value
      types via json.Marshal
        -> strings
        -> ints, floats
        -> booleans
        -> objects

--- json_encoder.go
+++ json_encoder.go
@@ -1,6 +1,7 @@
 package heka_clever_plugins
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/mozilla-services/heka/pipeline"
 	"strings"
@@ -36,9 +37,14 @@ func (pe *JsonEncoder) Init(config interface{}) (err error) {
 	return
 }
 
-// Encode parses a JSON payload. Given MessageFields in config, will overwrite values
-// using string-interpolation, similarly to PayloadRegexDecoder except with values from
-// the JSON instead of Regex matches.
+// Encode accepts a JSON payload and optionally rewrites fields via Populate message.
+// Given MessageFields in config, will overwrite values  using string-interpolation,
+// similarly to PayloadRegexDecoder except with values from the JSON instead of Regex matches.
+//
+// For example, given a JSON-payload of {"name":"world"}, one might write in the heka.toml
+// 		[encoderName.message_fields]
+// 		Payload="Hello, %name%"
+// which would output: "Hello, world"
 func (pe *JsonEncoder) Encode(pack *pipeline.PipelinePack) (output []byte, err error) {
 	// Parse payload. Fail unless JSON string.
 	payload := pack.Message.GetPayload()
@@ -47,23 +53,12 @@ func (pe *JsonEncoder) Encode(pack *pipeline.PipelinePack) (output []byte, err e
 		return nil, fmt.Errorf("string to encode is not JSON-parseable: %s", payload)
 	}
 
-	// For any field in the JSON that is a string, allow using its value
-	// as a "sub". Given JSON-payload of  {"name":"world"}, one might write
-	// 		[encoderName.message_fields]
-	// 		Payload="Hello, %name%"
-	subs := map[string]string{}
-	// allow subbing with string representation of values
-	// TODO: Refactor this to be shared in encoder/decoder
-	for key, val := range jsonMap {
-		stringVal, ok := val.(string)
-		if ok {
-			subs[key] = stringVal
-		} else {
-			// TODO: json dump, force to string
-		}
+	subs, err := forceInterfacesToStrings(jsonMap)
+	if err != nil {
+		return nil, err
 	}
 
-	// Interpolates the string using keys from subs
+	// interpolates the string using keys from subs
 	// https://github.com/mozilla-services/heka/blob/dev/pipeline/message_template.go#100
 	if err = pe.MessageFields.PopulateMessage(pack.Message, subs); err == nil {
 		output = []byte(*pack.Message.Payload)
@@ -76,6 +71,26 @@ func (pe *JsonEncoder) Encode(pack *pipeline.PipelinePack) (output []byte, err e
 	}
 
 	return
+}
+
+// forceInterfacesToStrings converts map[string]interface{} to map[string]string
+// Returns an error if fails to json marshal one of the interface{} values.
+func forceInterfacesToStrings(jsonMap map[string]interface{}) (subs map[string]string, err error) {
+	subs = map[string]string{}
+	for key, val := range jsonMap {
+		formattedVal, err := json.Marshal(val)
+		if err != nil {
+			return nil, err
+		}
+		stringVal := string(formattedVal)
+		if castVal, ok := val.(string); ok {
+			// JSON marshalled strings are surrounded in quotes - remove those
+			// by just using the string the results from casting the interface{}.(string)
+			stringVal = castVal
+		}
+		subs[key] = stringVal
+	}
+	return subs, nil
 }
 
 func init() {
