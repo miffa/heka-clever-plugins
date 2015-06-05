c27c0e0d71f7b68100b11d13a782efdc05cc448a
Nathan Leiby <nathan.leiby@clever.com>
2015-06-04 20:02:37 -0700

    filters: signalfxbatch- added docs.
    also moved max_count to top, with other user config

--- lua/filters/signalfxbatch.lua
+++ lua/filters/signalfxbatch.lua
@@ -1,3 +1,84 @@
+--[=[
+
+Extracts data from Fields in messages.
+Generates JSON suitable for send to datapoint SignalFx API.
+
+Batches multiple messages into one string, which can be passed to an HttpOutput
+using a PayloadEncoder.
+
+(Currently, only supports writing data points, NOT events.)
+
+Config:
+
+- metric_name (string, required)
+    String to use as the `metric` name in SignalFX.
+
+    Supports interpolation of field values from the processed message, using
+    `%{fieldname}`.
+
+    Any `fieldname` values of "Type", "Payload", "Hostname",
+    "Pid", "Logger", "Severity", or "EnvVersion" will be extracted from the
+    the base message schema, any other values will be assumed to refer to a
+    dynamic message field.
+
+    Only the first value of the first instance of a dynamic message field can be
+    used for series name interpolation. If the dynamic field doesn't exist, the
+    uninterpolated value will be left in the series name.
+
+    Note that it is not possible to interpolate either the
+    "Timestamp" or the "Uuid" message fields into the series name, those
+    values will be interpreted as referring to dynamic message fields.
+
+- value_field (string, optional, defaults to "value")
+    The `fieldname` to use as the value for the metric in signalfx. If the `value`
+    field is not present this encoder will set one as the value for counters: `1`.
+    A value of `0` will be used for `gauges`.
+
+- msg_type (string, optional, defaults to "signalfxbatch")
+    `Type` of the message outputted from this filter.
+
+- dimensions (string, optional, defaults to "")
+    A space delimited list of field names. Each of these will be written as
+    "dimensions" on the SignalFx data point, which you can filter by in SignalFx.
+    For example, a value of "Hostname Severity" would write the field
+
+        "dimensions": {
+            "Hostname": "my-hostname.example.com",
+            "Severity": 0
+        }
+
+    for the datapoint.
+
+
+- max_count (int, optional, defaults to 20)
+    Max number of messages before a batch is flushed from the filter.
+
+*Example Heka Configuration*
+
+.. code-block:: ini
+
+    [SignalfxBatchFilter]
+    message_matcher = "Fields[title] == 'your-metric' && Fields[metric_value] != NIL"
+    type = "SandboxFilter"
+    script_type = "lua"
+    filename = "lua/filters/signalfxbatch.lua"
+       [SignalfxBatchFilter.config]
+       metric_name = "test-metric.%{title}.%{Hostname}"
+       value_field = "metric_value"
+       dimensions = "Hostname Severity"
+       max_count = 5
+
+    [SignalfxHttpOutput]
+    message_matcher = "Fields[payload_name] == 'signalfxbatch'"
+    type = "HttpOutput"
+    encoder = "PayloadEncoder"
+    address = "https://ingest.signalfx.com/v2/datapoint"
+      [signalfx.headers]
+      content-type = ["application/json"]
+      X-SF-Token = "<YOUR-SIGNALFX-API-TOKEN>"
+
+--]=]
+
 require "cjson"
 require "string"
 require "table"
@@ -6,6 +87,7 @@ local metric_name = read_config("metric_name")
 local value_field = read_config("value_field") or "value"
 local dimensions_str = read_config("dimensions") or ""
 local msg_type = read_config("msg_type") or "signalfxbatch"
+local batch_max_count = read_config("max_count") or 20
 
 local use_subs
 if string.find(metric_name, "%%{[%w%p]-}") then
@@ -46,7 +128,6 @@ end
 
 local counters = {}
 local gauges = {}
-batch_max_count = read_config("max_count") or 20
 
 function flush()
    if #counters > 0 or #gauges > 0 then
