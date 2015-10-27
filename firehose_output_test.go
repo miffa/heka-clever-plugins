886e87fd886c60c47e7bc9356d126ba7b12e8604
Dylan Hurd <dylan.hurd@clever.com>
2015-10-27 20:39:54 +0000

    removed unused firehose test

--- firehose_output_test.go
+++ firehose_output_test.go
@@ -43,38 +43,3 @@ func TestRun(t *testing.T) {
 	err := firehoseOutput.Run(mockOR, mockPH)
 	assert.NoError(t, err, "did not expect err for valid Run()")
 }
-
-//func TestInvalidRun(t *testing.T) {
-//	mockCtrl := gomock.NewController(t)
-//	defer mockCtrl.Finish()
-//
-//	mockOR := pipelinemock.NewMockOutputRunner(mockCtrl)
-//	mockPH := pipelinemock.NewMockPluginHelper(mockCtrl)
-//	mockFirehose := NewMockFirehose(mockCtrl)
-//
-//	firehoseOutput := FirehoseOutput{
-//		client: mockFirehose,
-//	}
-//
-//	testChan := make(chan *pipeline.PipelinePack)
-//	mockOR.EXPECT().InChan().Return(testChan)
-//
-//	// Send test input through the channel
-//	input := `{"invalid:"value"}`
-//	go func() {
-//		testPack := pipeline.PipelinePack{
-//			Message: &message.Message{
-//				Payload: &input,
-//			},
-//		}
-//
-//		testChan <- &testPack
-//		close(testChan)
-//	}()
-//
-//	// cannot compare errors in this manner
-//	mockOR.EXPECT().LogError(errors.New("invalid character 'v' after object key"))
-//
-//	err := firehoseOutput.Run(mockOR, mockPH)
-//	assert.NoError(t, err, "did not expect err for valid Run()")
-//}
