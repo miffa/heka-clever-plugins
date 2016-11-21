b5b6bd80ed946d5ce2ca1359d3bb255a69067fb2
Jian Cheung <jian.cheung@clever.com>
2016-11-21 13:50:07 -0800

    test attempts to override container envs with empty values

--- lua/decoders/clever_container_decoder_spec.lua
+++ lua/decoders/clever_container_decoder_spec.lua
@@ -119,4 +119,26 @@ describe("Clever Container fields", function()
         assert.equals("apiservice", written_messages["Fields[container_app]"])
         assert.equals("abcd", written_messages["Fields[container_task]"])
     end)
+    it("Does not override container values with empty values", function()
+        mocks.reset()
+        require 'clever_container_decoder'
+
+        local mock_msg= {}
+        mock_msg['Timestamp'] = 2000000
+        mock_msg['Hostname'] = "hostname"
+        mock_msg.Fields = {}
+        mock_msg.Fields.programname = "docker/production--some-api/arn%3Aaws%3Aecs%3Aus-west-1%3A589690932525%3Atask%2F12345678-1234-1234-1234-1234567890ab"
+        mock_msg.Fields.container_env = ""
+        mock_msg.Fields.container_app = ""
+        mock_msg.Fields.container_task = ""
+        mocks.set_next_message(mock_msg)
+
+        assert.equals(process_message(), 0, "Should process_message successfully")
+        written_messages = mocks.written_messages()
+
+        assert.equals("production--some-api/12345678-1234-1234-1234-1234567890ab", written_messages["Fields[logtag]"])
+        assert.equals("production", written_messages["Fields[container_env]"])
+        assert.equals("some-api", written_messages["Fields[container_app]"])
+        assert.equals("12345678-1234-1234-1234-1234567890ab", written_messages["Fields[container_task]"])
+    end)
 end)
